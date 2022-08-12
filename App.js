1d0f980a50521e92b2c3e264372936ae066ddc4e
Nichol <n_pennell22@hotmail.com>
2022-08-11 23:00:30 -0400

    Initial commit: Added basic starting UI

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import DatePicker from 'react-native-date-picker';
 
 export default function App() {
+  let [startDate, setStartDate] = useState(new Date())
+  const [open, setOpen] = useState(false)
+  const [resultsNum, setResultsNum] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.titleHeader}>
+        <Text style={styles.titleText}>Picture of the day</Text>
+      </View>
+      <View style={styles.bodyContainer}>
+        <Text style={styles.subHeaderText}>Picture of the day:</Text>
+        <Text style={styles.bodyText}>Search for Astronomy: Picture of the day by date.</Text>
+        <TextInput 
+          placeholder="Start Date"
+          paddingVertical="5%"
+          style={styles.dateInput}
+        />
+        <TextInput 
+          placeholder="End Date"
+          paddingVertical="5%"
+          style={styles.dateInput}
+        />
+        <TouchableOpacity>
+          <Text>Search</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.resultsContainer}>
+        <Text>Results ({resultsNum}):</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +42,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  titleHeader: {
+    justifyContent: 'center', 
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: '15%',
+  },
+  titleText: {
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  bodyContainer: {
+    padding: '5%',
+    paddingTop: '10%'
+  },
+  subHeaderText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  bodyText: {
+    fontSize: 15,
+    paddingTop: '2%'
+  },
+  dateInput: {
+    height: '20%',
+    width: '100%',
+    // backgroundColor: '#E0EAE5',
+    color: 'black',
+  },
+  resultsContainer: {
+    padding: '5%',
+    // paddingTop: '1%'
   },
 });
